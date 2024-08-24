0b98c76480b481eab6b0eb2b473985e3de438a55
Jhonata.Brito <jhonatawwxx@gmail.com>
2024-08-24 18:43:45 -0300

    feat: desambiguação de beans com @Primary

--- src/main/java/com/restaurante/api/service/AtivacaoClienteService.java
+++ src/main/java/com/restaurante/api/service/AtivacaoClienteService.java
@@ -1,20 +1,21 @@
 package com.restaurante.api.service;
+
 import com.restaurante.api.modelo.Cliente;
 import com.restaurante.api.notificacao.Notificador;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class AtivacaoClienteService {
 
-    private List<Notificador> notificadores;
+    private Notificador notificador;
+
+    public AtivacaoClienteService(Notificador notificador) {
+        this.notificador = notificador;
+    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
-        for (Notificador notificador : notificadores) {
-            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-        }
+        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
+
 }
