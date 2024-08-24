9c523ef91562b5685c2d247063e5c7455ad709c3
Jhonata.Brito <jhonatawwxx@gmail.com>
2024-08-24 20:29:40 -0300

    feat: criando e acessando propriedades customizadas com @Value pegando de valores de application.properties

--- src/main/java/com/restaurante/api/notificacao/NotificadorEmail.java
+++ src/main/java/com/restaurante/api/notificacao/NotificadorEmail.java
@@ -3,6 +3,7 @@ package com.restaurante.api.notificacao;
 import com.restaurante.api.config.TipoDoNotificador;
 import com.restaurante.api.modelo.Cliente;
 import com.restaurante.api.modelo.NivelUrgencia;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +11,20 @@ import org.springframework.stereotype.Component;
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador {
+
+    @Value("${notificador.email.host}")
+    private String hostServidorSmtp;
+
+    @Value("${notificador.email.port}")
+    private String portaServidorSmtp;
+
     public NotificadorEmail() {
         System.out.println("NotificadorEmail REAL");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host SMTP: " + this.hostServidorSmtp);
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
