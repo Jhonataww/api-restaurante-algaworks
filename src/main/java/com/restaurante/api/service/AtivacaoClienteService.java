444ae1fefaf5bab06dd52860fdb2df7d55d451ab
Jhonata.Brito <jhonatawwxx@gmail.com>
2024-08-24 19:57:35 -0300

    feat: publicando e consumindo eventos customizados

--- src/main/java/com/restaurante/api/service/AtivacaoClienteService.java
+++ src/main/java/com/restaurante/api/service/AtivacaoClienteService.java
@@ -1,34 +1,20 @@
 package com.restaurante.api.service;
 
-import com.restaurante.api.config.TipoDoNotificador;
+import com.restaurante.api.events.ClienteAtivoEvent;
 import com.restaurante.api.modelo.Cliente;
-import com.restaurante.api.modelo.NivelUrgencia;
-import com.restaurante.api.notificacao.Notificador;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class AtivacaoClienteService {
 
-    @TipoDoNotificador(NivelUrgencia.NORMAL)
     @Autowired
-    private Notificador notificador;
-
-    //@PostConstruct
-    public void init(){
-        System.out.println("init");
-    }
-
-    //@PreDestroy
-    public void destroy(){
-        System.out.println("destroy");
-    }
+    private ApplicationEventPublisher eventPublisher; //publicador de evento
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        eventPublisher.publishEvent(new ClienteAtivoEvent(cliente)); //publica o evento
     }
 
 }
