171309cc8a4b75daf422a86d77420aedcc2ebb39
Jhonata.Brito <jhonatawwxx@gmail.com>
2024-08-24 18:38:53 -0300

    feat: ambiguidade de beans e injeção de lista de beans

--- src/main/java/com/restaurante/api/service/AtivacaoClienteService.java
+++ src/main/java/com/restaurante/api/service/AtivacaoClienteService.java
@@ -4,23 +4,17 @@ import com.restaurante.api.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class AtivacaoClienteService {
 
-    private Notificador notificador;
-
-    //pontos de injeção - não é muito recomendado usar - usar construtor
-    @Autowired
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-    }
-
-//    public AtivacaoClienteService(String hostServidorSmtp) {
-//    }
+    private List<Notificador> notificadores;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        for (Notificador notificador : notificadores) {
+            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        }
     }
-
 }
