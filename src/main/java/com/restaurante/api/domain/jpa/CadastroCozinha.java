740ae3009013ffc625c17e115d7e6c821732ee98
Jhonata Williams <jhonatawwxx@gmail.com>
2024-08-25 12:29:24 -0300

    feat: consultando objetos do banco de dados

---
+++ src/main/java/com/restaurante/api/domain/jpa/CadastroCozinha.java
@@ -0,0 +1,21 @@
+package com.restaurante.api.domain.jpa;
+
+import com.restaurante.api.domain.model.Cozinha;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CadastroCozinha {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    List<Cozinha> listar (){
+        TypedQuery<Cozinha> query = manager.createQuery("from Cozinha", Cozinha.class); //cria uma consulta pra cozinha
+        return query.getResultList(); //retorna a lista de cozinhas
+    }
+}
