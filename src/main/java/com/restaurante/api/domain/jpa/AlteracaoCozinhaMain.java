d2c70085c1888433e164a8730b9395f14108a38f
Jhonata Williams <jhonatawwxx@gmail.com>
2024-08-25 12:53:20 -0300

    feat: atualizando um objeto no banco de dados JPA puro

--- src/main/java/com/restaurante/api/domain/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/restaurante/api/domain/jpa/AlteracaoCozinhaMain.java
@@ -6,7 +6,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
 
     public static void main(String[] args) {
 
@@ -18,20 +18,10 @@ public class InclusaoCozinhaMain {
 
         CadastroCozinha cadastroCozinha = configurableApplicationContext.getBean(CadastroCozinha.class);
 
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setNome("Chinesa");
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Chinesa");
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Marroquina");
-
-        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-        System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
-
-        cozinha1 = cadastroCozinha.salvar(cozinha1);
-        cozinha2 = cadastroCozinha.salvar(cozinha2);
-
-        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-        System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
-
+        cadastroCozinha.salvar(cozinha);
     }
 }
