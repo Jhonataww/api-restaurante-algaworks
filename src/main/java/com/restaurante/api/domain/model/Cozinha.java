75d79c8f858dc0a5568d9526f2a6c318dac0e0b6
Jhonata Williams <jhonatawwxx@gmail.com>
2024-08-25 11:54:00 -0300

    feat: criando as tabelas do banco a partir das entidades

---
+++ src/main/java/com/restaurante/api/domain/model/Cozinha.java
@@ -0,0 +1,48 @@
+package com.restaurante.api.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "TAB_COZINHAS")
+public class Cozinha {
+
+    @Id
+    private Long id;
+
+    @Column(name = "NOME_COZINHA", length = 30)
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cozinha cozinha = (Cozinha) o;
+        return Objects.equals(id, cozinha.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/restaurante/api/domain/model/Restaurante.java
@@ -0,0 +1,46 @@
+package com.restaurante.api.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "TAB_RESTAURANTES")
+public class Restaurante {
+
+    @Id
+    private Long id;
+
+    @Column(name = "NOME_RESTAURANTE", length = 30)
+    private String nome;
+
+    @Column(name = "TAXA_FRETE")
+    private BigDecimal taxaFrete;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getTaxaFrete() {
+        return taxaFrete;
+    }
+
+    public void setTaxaFrete(BigDecimal taxaFrete) {
+        this.taxaFrete = taxaFrete;
+    }
+}
