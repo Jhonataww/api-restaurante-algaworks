0de81bc1d97e96c3c66a4595200827bb8105e895
Jhonata Williams <jhonatawwxx@gmail.com>
2024-08-26 17:47:33 -0300

    feat: implementando content negotiation para retornar JSON ou XML

--- src/main/java/com/restaurante/api/controller/CozinhaController.java
+++ src/main/java/com/restaurante/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.restaurante.api.controller;
 import com.restaurante.api.domain.model.Cozinha;
 import com.restaurante.api.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,15 @@ import java.util.List;
 //@Controller //anotação de container spring
 //@ResponseBody  //informa que a resposta do método será o corpo da resposta
 @RestController //anotação que informa que a classe é um controlador rest (possui controller e responsebodyu)
+//@RequestMapping(value = "/cozinhas", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}) // Geral no recurso - mapeamento da requisição e especificando o produces para que o método possa retornar tanto JSON quanto XML
 @RequestMapping("/cozinhas") //mapeamento da requisição
 public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
+    //Especificando o produces para que o método possa retornar tanto JSON quanto XML
+    @GetMapping(produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}) //mapeamento do método
     public List<Cozinha> listar(){
        return cozinhaRepository.listar();
     }
