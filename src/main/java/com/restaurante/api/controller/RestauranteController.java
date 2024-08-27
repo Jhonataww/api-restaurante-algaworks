773881648359193098802d8ddef2cff1e4983c21
Jhonata Williams <jhonatawwxx@gmail.com>
2024-08-27 19:34:41 -0300

    chore: limpando dependencias não usadas

--- src/main/java/com/restaurante/api/controller/RestauranteController.java
+++ src/main/java/com/restaurante/api/controller/RestauranteController.java
@@ -6,7 +6,6 @@ import com.restaurante.api.domain.exception.EntidadeNaoEncontradaException;
 import com.restaurante.api.domain.model.Restaurante;
 import com.restaurante.api.domain.repository.RestauranteRepository;
 import com.restaurante.api.domain.service.CadastroRestauranteService;
-import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/restaurantes")
