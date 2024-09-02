597ae07865cd842f1de962848008d078762b0703
Jhonata Williams <jhonatawwxx@gmail.com>
2024-09-02 19:47:26 -0300

    feat: criando consultas com query methods

--- src/main/java/com/restaurante/api/domain/repository/CozinhaRepository.java
+++ src/main/java/com/restaurante/api/domain/repository/CozinhaRepository.java
@@ -4,9 +4,11 @@ import com.restaurante.api.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
+    List<Cozinha> findByNome(String nome);
 
-   // List<Cozinha> consultarPorNome(String nome);
 }
