73c44d2d34c7bd0e5d9942def366e96aaac30081
Jhonata Williams <jhonatawwxx@gmail.com>
2024-09-02 19:57:17 -0300

    feat: usando as keywords para definir critérios de query methods

--- src/main/java/com/restaurante/api/domain/repository/CozinhaRepository.java
+++ src/main/java/com/restaurante/api/domain/repository/CozinhaRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-    List<Cozinha> findByNome(String nome);
+    List<Cozinha> findByNomeContaining(String nome);  //busca like % nome % Query words method
 
+    Optional<Cozinha> findByNome(String nome);
 }
