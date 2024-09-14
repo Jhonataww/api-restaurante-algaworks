6bdc8ce846daebecda1bf676999737e3529f306d
Jhonata.Brito <jhonatawwxx@gmail.com>
2024-09-14 19:43:48 -0300

    feat: implementando uma consulta dinâmica com JPQL

--- src/main/java/com/restaurante/api/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/restaurante/api/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -3,28 +3,50 @@ package com.restaurante.api.infrastructure.repository;
 import com.restaurante.api.domain.model.Restaurante;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
 public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {    //usar Impl no final do nome da classe para indicar que é uma implementação de uma interface
 
-
     @PersistenceContext
     private EntityManager manager;
 
-
     @Override
-    public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+    public List<Restaurante> find(String nome,
+                                  BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 
-            var jpql = "from Restaurante where nome like :nome and taxaFrete between :taxaInicial and :taxaFinal";
+        var jpql = new StringBuilder();
+        jpql.append("from Restaurante where 0 = 0 "); //para não precisar de ifs para concatenar o where
 
-            return manager.createQuery(jpql, Restaurante.class)
-                    .setParameter("nome", "%" + nome + "%")
-                    .setParameter("taxaInicial", taxaFreteInicial)
-                    .setParameter("taxaFinal", taxaFreteFinal)
-                    .getResultList();
+        var parametros = new HashMap<String, Object>(); //será usado para passar os parâmetros para a query
+
+        if (StringUtils.hasLength(nome)) {
+            jpql.append("and nome like :nome "); //monta a query de acordo com os parâmetros passados
+            parametros.put("nome", "%" + nome + "%");
+        }
+
+        if (taxaFreteInicial != null) {
+            jpql.append("and taxaFrete >= :taxaInicial ");
+            parametros.put("taxaInicial", taxaFreteInicial);
+        }
+
+        if (taxaFreteFinal != null) {
+            jpql.append("and taxaFrete <= :taxaFinal ");
+            parametros.put("taxaFinal", taxaFreteFinal);
+        }
+
+        TypedQuery<Restaurante> query = manager //typedQuery é uma query que retorna um tipo específico
+                .createQuery(jpql.toString(), Restaurante.class);
+
+        parametros.forEach((chave, valor) -> query.setParameter(chave, valor)); //lambda para passar os parâmetros para a query
+
+        return query.getResultList();
     }
+
 }
