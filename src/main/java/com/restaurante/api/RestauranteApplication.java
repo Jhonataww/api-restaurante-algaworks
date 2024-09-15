f6ab814283537bbdde43b21b77e9528ccd521aba
Jhonata.Brito <jhonatawwxx@gmail.com>
2024-09-14 22:42:51 -0300

    feat: Estendendo o JpaRepository para customizar o repositório base

--- src/main/java/com/restaurante/api/RestauranteApplication.java
+++ src/main/java/com/restaurante/api/RestauranteApplication.java
@@ -1,9 +1,12 @@
 package com.restaurante.api;
 
+import com.restaurante.api.infrastructure.repository.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class RestauranteApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/restaurante/api/infrastructure/repository/CustomJpaRepositoryImpl.java
@@ -0,0 +1,23 @@
+package com.restaurante.api.infrastructure.repository;
+
+import com.restaurante.api.domain.repository.CustomJpaRepository;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import java.util.Optional;
+
+public class CustomJpaRepositoryImpl<T, ID>  extends SimpleJpaRepository<T, ID> implements CustomJpaRepository<T, ID> {
+
+    private EntityManager manager;
+
+    public CustomJpaRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
+        super(entityInformation, entityManager);
+    }
+
+    @Override
+    public Optional<T> buscarPrimeiro() {
+        return Optional.empty();
+    }
+}
